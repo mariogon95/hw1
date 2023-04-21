6a8a2adca8ac30ca2e2aa7db29c6f45f3020f877
Mario Gonzalez <mario.gonzalez@kellogg.northwestern.edu>
2023-04-21 00:06:35 +0000

    hw1_finished

--- kmdb.sql
+++ kmdb.sql
@@ -114,19 +114,91 @@
 -- TODO!
 
 -- Prints a header for the movies output
-.print "Movies"
-.print "======"
-.print ""
+--.print "Movies"
+--.print "======"
+--.print ""
 
 -- The SQL statement for the movies output
 -- TODO!
 
 -- Prints a header for the cast output
-.print ""
-.print "Top Cast"
-.print "========"
-.print ""
+--.print ""
+--.print "Top Cast"
+--.print "========"
+--.print ""
 
 
 -- The SQL statement for the cast output
 -- TODO!
+
+DROP TABLE IF EXISTS actors;
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS characters;
+
+CREATE TABLE actors (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT
+);
+
+CREATE TABLE movies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title TEXT,
+  year INTEGER,
+  rating_letter TEXT,
+  studio TEXT
+);
+
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  character_name TEXT,
+  actor_id INTEGER,
+  movie_id INTEGER
+);
+
+INSERT INTO actors (name) VALUES("Christian Bale");
+INSERT INTO actors (name) VALUES("Michael Caine");
+INSERT INTO actors (name) VALUES("Liam Neeson");
+INSERT INTO actors (name) VALUES("Katie Holmes");
+INSERT INTO actors (name) VALUES("Gary Oldman");
+INSERT INTO actors (name) VALUES("Heath Ledger");
+INSERT INTO actors (name) VALUES("Aaron Eckhart");
+INSERT INTO actors (name) VALUES("Maggie Gyllenhaal");
+INSERT INTO actors (name) VALUES("Tom Hardy");
+INSERT INTO actors (name) VALUES("Joseph Gordon-Levitt");
+INSERT INTO actors (name) VALUES("Anne Hathaway");
+
+INSERT INTO movies (title, year, rating_letter, studio) VALUES ('Batman Begins', 2005, "PG-13", "Warner Bros.");
+INSERT INTO movies (title, year, rating_letter, studio) VALUES ('The Dark Knight', 2008, "PG-13", "Warner Bros.");
+INSERT INTO movies (title, year, rating_letter, studio) VALUES ('The Dark Knight Rises', 2012, "PG-13", "Warner Bros.");
+
+INSERT INTO characters (character_name, actor_id, movie_id) VALUES ("Bruce Wayne", 1, 1);
+INSERT INTO characters (character_name, actor_id, movie_id) VALUES ("Alfred", 2, 1);
+INSERT INTO characters (character_name, actor_id, movie_id) VALUES ("Ra's Al Ghul", 3, 1);
+INSERT INTO characters (character_name, actor_id, movie_id) VALUES ("Rachel Dawes", 4, 1);
+INSERT INTO characters (character_name, actor_id, movie_id) VALUES ("Comissioner Gordon", 5, 1);
+INSERT INTO characters (character_name, actor_id, movie_id) VALUES ("Bruce Wayne", 1, 2);
+INSERT INTO characters (character_name, actor_id, movie_id) VALUES ("Joker", 6, 2);
+INSERT INTO characters (character_name, actor_id, movie_id) VALUES ("Harvey Dent", 7, 2);
+INSERT INTO characters (character_name, actor_id, movie_id) VALUES ("Alfred", 2, 2);
+INSERT INTO characters (character_name, actor_id, movie_id) VALUES ("Rachel Dawes", 8, 2);
+INSERT INTO characters (character_name, actor_id, movie_id) VALUES ("Bruce Wayne", 1, 3);
+INSERT INTO characters (character_name, actor_id, movie_id) VALUES ("Comissioner Gordon", 5, 3);
+INSERT INTO characters (character_name, actor_id, movie_id) VALUES ("Bane", 9, 3);
+INSERT INTO characters (character_name, actor_id, movie_id) VALUES ("John Blake", 10, 3);
+INSERT INTO characters (character_name, actor_id, movie_id) VALUES ("Selina Kyle", 11, 3);
+
+.print "Movies"
+.print "======"
+.print ""
+SELECT title, year, rating_letter, studio
+FROM movies
+;
+
+.print ""
+.print "Top Cast"
+.print "========"
+.print ""
+SELECT title, name, character_name
+FROM characters INNER JOIN movies ON movies.id = movie_id
+INNER JOIN actors ON actors.id = actor_id
+;
